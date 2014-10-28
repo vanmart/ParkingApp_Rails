eefdf5d2aa9f57ae7bd954a25f936d8adce0a71c
vanmart <vanmartc@gmail.com>
2014-10-28 18:04:26 -0500

    update devise views with twitter bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
 
     unauthenticated :user do
-      root to: "devise/sessions#new", as: :unauthenticated_root
+      root to: "home#index", as: :unauthenticated_root
     end
   end
 
